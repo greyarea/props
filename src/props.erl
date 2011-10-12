b847fc23fce96e5d682d3534a134ffdf6c612933
Jack Moffitt <jack@metajack.im>
2011-10-12 14:52:40 -0600

    Initial sketch of public API.

---
+++ src/props.erl
@@ -0,0 +1,42 @@
+%% @doc Property structure library
+-module(props).
+
+-export([new/0,
+         get/2,
+         get/3,
+         set/3,
+         diff/2]).
+
+-export_type([prop_value/0, props/0, prop_path/0]).
+
+-type prop_value() :: true | false | number() | 
+                      [prop_value() | props()] | props().
+-opaque props() :: {[{binary(), prop_value()}]}.
+
+-type prop_path() :: atom() | string().
+
+%% @doc Create a new props structure.
+-spec new() -> props().
+new() ->
+    {[]}.
+
+%% @doc Get a property for a props structure.
+%% This is equivalent to `get(PathSpec, Props, undefined)`.
+-spec get(prop_path(), props()) -> undefined | prop_value().
+get(_Path, _Props) ->
+    undefined.
+
+%% @doc Get a property in props structure by path.
+-spec get(prop_path(), props(), undefined | prop_value()) -> undefined | prop_value().
+get(_Path, _Props, Default) ->
+    Default.
+
+%% @doc Set a property in a props structure by path.
+-spec set(prop_path(), prop_value(), props()) -> props().
+set(_Path, _Value, Props) ->
+    Props.
+
+%% @doc Return a list of differences between two property structures.
+-spec diff(props(), props()) -> [{prop_path(), {prop_value(), prop_value()}}].
+diff(_Props1, _Props2) ->
+    [].
